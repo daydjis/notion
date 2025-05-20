4988786a378a3aca7a864e37954d6797c7209eb2
mironiukia <ilia.mironyuk21@gmail.com>
2025-05-20 20:23:17 +0400

    Task-1

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -1,1 +1,98 @@
 package handler
+
+import (
+	jwt "github.com/appleboy/gin-jwt/v2"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"todo-api/internal/model"
+	"todo-api/internal/service"
+)
+
+type UserHandler struct {
+	Service service.UserService
+}
+
+func NewUserHandler(service service.UserService) *UserHandler {
+	return &UserHandler{Service: service}
+}
+
+func (h *UserHandler) RegisterRoutes(r *gin.Engine) {
+	r.POST("/register", h.registerUser)
+	r.POST("/login", h.loginUser)
+	r.DELETE("/user/:id", h.deleteUser)
+	r.GET("/users", h.getAllUsers)
+	r.GET("/user/:id", h.getUserInfo)
+}
+
+// registerUser handles new user registration
+func (h *UserHandler) registerUser(c *gin.Context) {
+	var input model.RegisterInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user, err := h.Service.Register(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, user)
+}
+
+// loginUser handles user authentication and returns a token
+func (h *UserHandler) loginUser(c *gin.Context) {
+	var input model.LoginInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	token, err := h.Service.Login(input)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
+
+// getAllUsers returns a list of all users
+func (h *UserHandler) getAllUsers(c *gin.Context) {
+	users, err := h.Service.GetAllUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, users)
+}
+
+// getUserInfo returns details of a single user by ID
+func (h *UserHandler) getUserInfo(c *gin.Context) {
+	claims := jwt.ExtractClaims(c)
+
+	id, err := strconv.Atoi(claims["id"].(string))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid user id"})
+		return
+	}
+	user, err := h.Service.GetUser(uint(id))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+// deleteUser removes a user by ID
+func (h *UserHandler) deleteUser(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid user id"})
+		return
+	}
+	if err := h.Service.DeleteUser(uint(id)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.Status(http.StatusNoContent)
+}
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,1 +1,88 @@
 package service
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"todo-api/internal/model"
+	"todo-api/internal/repository"
+)
+
+type UserService interface {
+	CreateUser(input *model.UserInput) (*model.UserInput, error)
+	GetUser(id uint) (*model.User, error)
+	GetAllUsers() ([]model.User, error)
+	UpdateUser(input *model.User) (*model.User, error)
+	DeleteUser(id uint) error
+	Register(input model.RegisterInput) (*model.RegisterInput, error)
+	Login(user model.LoginInput) (*model.User, error)
+}
+
+type userService struct {
+	Repo repository.UserRepository
+}
+
+func NewUserService(repo repository.UserRepository) UserService {
+	return &userService{Repo: repo}
+}
+
+func (s *userService) CreateUser(input *model.UserInput) (*model.UserInput, error) {
+	if err, _ := s.Repo.Create(input); err != nil {
+		return nil, fmt.Errorf("failed to create task: %w", err)
+	}
+	return input, nil
+}
+
+func (s *userService) GetUser(id uint) (*model.User, error) {
+	task, err := s.Repo.GetByID(id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get task: %w", err)
+	}
+	return task, nil
+}
+
+func (s *userService) GetAllUsers() ([]model.User, error) {
+	tasks, err := s.Repo.GetAll()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get tasks: %w", err)
+	}
+	return tasks, nil
+}
+
+func (s *userService) Register(input model.RegisterInput) (*model.RegisterInput, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	u := &model.RegisterInput{
+		Name:     input.Name,
+		Password: string(hashed),
+	}
+	res, err := s.Repo.Create((*model.UserInput)(u))
+	return (*model.RegisterInput)(res), err
+}
+
+func (s *userService) Login(input model.LoginInput) (*model.User, error) {
+
+	u, err := s.Repo.GetByName(input.Name)
+	if err != nil {
+		return nil, err
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(input.Password)); err != nil {
+		return nil, fmt.Errorf("invalid credentials")
+	}
+	return u, nil
+}
+
+func (s *userService) UpdateUser(input *model.User) (*model.User, error) {
+	if err := s.Repo.Update(input); err != nil {
+		return nil, fmt.Errorf("failed to update task: %w", err)
+	}
+	return input, nil
+}
+
+func (s *userService) DeleteUser(id uint) error {
+	if err := s.Repo.Delete(id); err != nil {
+		return fmt.Errorf("failed to delete task: %w", err)
+	}
+	return nil
+}
